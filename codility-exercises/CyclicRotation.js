a5f3648701e0a6426b5dd8a0378b7ccde08b560d
keff6 <kev.fallas@gmail.com>
2018-08-12 19:09:42 -0600

    Add codility CyclicRotation solution

---
+++ codility-exercises/CyclicRotation.js
@@ -0,0 +1,51 @@
+/*
+Cyclic Rotation
+
+Write a function:
+
+function solution(A, K);
+
+that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
+
+For example, given
+
+    A = [3, 8, 9, 7, 6]
+    K = 3
+the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+
+    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+    [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+    [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+For another example, given
+
+    A = [0, 0, 0]
+    K = 1
+the function should return [0, 0, 0]
+
+Given
+
+    A = [1, 2, 3, 4]
+    K = 4
+the function should return [1, 2, 3, 4]
+
+Assume that:
+
+N and K are integers within the range [0..100];
+each element of array A is an integer within the range [−1,000..1,000].
+In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
+*/
+
+/*-------------------------------------------
+Solution Score: 100%
+--------------------------------------------*/
+
+function solution(A, K) {
+  if (A.length > 0) {
+      for (let i = 0; i < K; i++) {
+          const toRotate = A[A.length - 1];
+          A.pop();    
+          A.unshift(toRotate);
+      }
+  }
+  return A;
+}
