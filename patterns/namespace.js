04a8d56dfe20efeec26e2e2ef6f851ebf008da45
keff6 <kev.fallas@gmail.com>
2018-09-09 17:13:02 -0600

    Update namespace pattern info

--- patterns/namespace.js
+++ patterns/namespace.js
@@ -4,6 +4,17 @@ The idea of this pattern in to create a very unique path, that will be
 specific for your project to avoid using the global scope.
 It's similar to object literal, but more specific
 
+Pros:
++ Helps reduce the number of globals required by our programs
++ Help avoid naming collissions
++ Help avoid excessive name prefixing
+
+Cons:
+- More to type
+- Only one global instance means that any part of the code can modify it
+- Long nested names means longer property resolution lookup
+
+
 Tests
 > myPc.projects.javascript.showMessage(); // Hello
 > myPc.projects.javascript.greetMe("your name")
