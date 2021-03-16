a54eb51ea89a55c2ac3b9eb7319e613fa76f9f40
keff6 <kev.fallas@gmail.com>
2021-03-16 13:43:17 -0600

    Add heap extractMax method

---
+++ data-structures/BinaryHeap/README.md
@@ -0,0 +1,13 @@
+# Binary Heap
+
+## Description
+Heaps are an specific form of tree
+Its very similar to a binary search tree but it has different rules
+In a maxBinaryHeap the parent nodes are always larger than child nodes
+In a minBinaryHeap the parent nodes are always smaller than child nodes
+rules of max
+each parent has at most two nodes
+the parent is greater than the children but there are no guarantees between siblings
+is compact as possible
+all the children of each node are as full as they can be and left children
+are filled out first
--- data-structures/Heap/Heap.js
+++ data-structures/Heap/Heap.js
@@ -25,6 +25,50 @@ class MaxBinaryHeap {
       parentIndex = Math.floor((index - 1)/2);
     }
   }
+
+  sinkDown() {
+    let idx = 0;
+    const len = this.values.length;
+    const el = this.values[0];
+    while(true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if(leftChildIdx < len) {
+        leftChild = this.values[leftChildIdx];
+        if(leftChild < el) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if(rightChildIdx < len) {
+        rightChild = this.values[rightChildIdx];
+        if((swap === null && rightChild > el) || (swap !== null && rightChild > leftChild)){
+          swap = rightChildIdx;
+        }
+      }
+
+      if(swap === null) break;
+      this.values[idx] = this.values[swap];
+    }
+  }
+
+  extractMax() {
+    const len = this.values.length;
+    // Swap the first value in the values with the last one
+    [this.values[0], this.values[len - 1]] = [this.values[len - 1], this.values[0]];
+    // Pop the max value
+    const max = this.values.pop();
+    // Sink down the new root to its right place
+    if(len > 0) {
+      this.sinkDown();
+    }
+    
+    return max;
+
+  }
 }
   
   
