83d2243031d00262a71dbab78086ca878d4da3f0
keff6 <kev.fallas@gmail.com>
2021-05-24 20:56:54 -0600

    Add test weighted graph

--- data-structures/Graph/WeightedGraph.js
+++ data-structures/Graph/WeightedGraph.js
@@ -12,3 +12,22 @@ class WeightedGraph {
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
   }
 }
+
+const y = new WeightedGraph();
+
+y.addVertex('A')
+y.addVertex('B')
+y.addVertex('C')
+y.addVertex('D')
+y.addVertex('E')
+y.addVertex('F')
+y.addEdge('A', 'B', 4)
+y.addEdge('A', 'C', 2)
+y.addEdge('B', 'E', 3)
+y.addEdge('C', 'D', 2)
+y.addEdge('C', 'F', 4)
+y.addEdge('D', 'E', 3)
+y.addEdge('D', 'F', 1)
+y.addEdge('F', 'E', 1)
+
+console.log(y)
